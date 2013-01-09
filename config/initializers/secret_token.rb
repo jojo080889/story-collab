9fca3d120ae43b9a53ac2d98149e5234a6752b4e
Joy Kim <jojo080889@gmail.com>
2013-01-08 17:33:20 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryCollab::Application.config.secret_token = '932d243bf2a3da222e46a93df4e4e72bde6d101938054dfba1eb1fdac84919fd4cf17f0207fb684df501f36008d1a385ff1fe2faf9b3fa45adbf3b3a5d9bffe9'
